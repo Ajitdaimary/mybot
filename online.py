02c784ed3c7ff48bbd2e207ede8784131d7ba261
Marcos <45558399+Ajitdaimary@users.noreply.github.com>
2018-12-07 14:40:15 +0530

    Update online.py

--- online.py
+++ online.py
@@ -21,7 +21,7 @@ async def on_ready():
 	print('--------')
 	print('--------')
 	print('Started marcos') #add_your_bot_name_here
-	return await client.change_presence(game=discord.Game(name='KELLING MARCOS')) #add_your_bot_status_here
+	return await client.change_presence(game=discord.Game(name='KELLING BY MARCOS')) #add_your_bot_status_here
 	
 def is_owner(ctx):
     return ctx.message.author.id == "472680171451973632,485868646854557696" #replace_it_with_your_discord_id
