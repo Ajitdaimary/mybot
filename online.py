0bf88da3ac25ec6ebc17cb79017fdff29447ebbb
badsahah <45558399+Ajitdaimary@users.noreply.github.com>
2018-12-13 12:09:42 +0530

    Update online.py

--- online.py
+++ online.py
@@ -383,4 +383,173 @@ async def meme(ctx):
     await client.send_message(ctx.message.channel, embed=embed) 
 
 
+@client.command(pass_context=True)  
+@commands.has_permissions(ban_members=True)      
+async def ban(ctx,user:discord.Member):
+    if user.server_permissions.ban_members:
+      await client.say('**He is mod/admin and i am unable to ban him/her**')
+      return
+    else:
+      await client.ban(user)
+      await client.say(user.name+' was banned. Good bye '+user.name+'!')
+      for channel in member.server.channels:
+        if channel.name == 'information-log':
+            embed=discord.Embed(title="User banned!", description="**{0}** banned by **{1}**!".format(member, ctx.message.author), color=0x38761D)
+            await client.send_message(channel, embed=embed)
+
+
+@client.command(pass_context=True)  
+@commands.has_permissions(ban_members=True)     
+async def unban(ctx):
+    ban_list = await client.get_bans(ctx.message.server)
+
+    # Show banned users
+    await client.say("Ban list:\n{}".format("\n".join([user.name for user in ban_list])))
+
+    # Unban last banned user
+    if not ban_list:
+      await client.say('Ban list is empty.')
+      return
+    else:
+      await client.unban(ctx.message.server, ban_list[-1])
+      await client.say('Unbanned user: `{}`'.format(ban_list[-1].name))
+      for channel in member.server.channels:
+        if channel.name == 'soyal-log':
+            embed=discord.Embed(title="User unbanned!", description="**{0}** unbanned by **{1}**!".format(ban_list[-1].name, ctx.message.author), color=0x38761D)
+            await client.send_message(channel, embed=embed)
+
+
+@client.command(pass_context=True)  
+@commands.has_permissions(kick_members=True)     
+
+async def serverinfo(ctx):
+    '''Displays Info About The Server!'''
+
+    server = ctx.message.server
+    roles = [x.name for x in server.role_hierarchy]
+    role_length = len(roles)
+
+    if role_length > 50: #Just in case there are too many roles...
+        roles = roles[:50]
+        roles.append('>>>> Displaying[50/%s] Roles'%len(roles))
+
+    roles = ', '.join(roles);
+    channelz = len(server.channels);
+    time = str(server.created_at); time = time.split(' '); time= time[0];
+    r, g, b = tuple(int(x * 255) for x in colorsys.hsv_to_rgb(random.random(), 1, 1))
+    join = discord.Embed(description= '%s '%(str(server)),title = 'Server Name', color = discord.Color((r << 16) + (g << 8) + b));
+    join.set_thumbnail(url = server.icon_url);
+    join.add_field(name = '__Owner__', value = str(server.owner) + '\n' + server.owner.id);
+    join.add_field(name = '__ID__', value = str(server.id))
+    join.add_field(name = '__Member Count__', value = str(server.member_count));
+    join.add_field(name = '__Text/Voice Channels__', value = str(channelz));
+    join.add_field(name = '__Roles (%s)__'%str(role_length), value = roles);
+    join.set_footer(text ='Created: %s'%time);
+
+    return await client.say(embed = join);
+
+
+
+@client.command(pass_context = True)
+@commands.has_permissions(administrator=True)     
+async def makemod(ctx, user: discord.Member):
+    nickname = '[̲̅M̲̅]' + user.name
+    await client.change_nickname(user, nickname=nickname)
+    role = discord.utils.get(ctx.message.server.roles, name='Moderator')
+    await client.add_roles(user, role)
+    r, g, b = tuple(int(x * 255) for x in colorsys.hsv_to_rgb(random.random(), 1, 1))
+    embed = discord.Embed(color = discord.Color((r << 16) + (g << 8) + b))
+    embed.set_author(name='Congratulations Message')
+    embed.add_field(name = '__Congratulations__',value ='**Congratulations for mod.Hope you will be more active here. Thanks for your help and support.**',inline = False)
+    embed.set_image(url = 'https://preview.ibb.co/i1izTz/ezgif_5_e20b665628.gif')
+    await client.send_message(user,embed=embed)
+    await client.delete_message(ctx.message)
+
+
+@client.command(pass_context = True)
+@commands.has_permissions(administrator=True)     
+async def makeadmin(ctx, user: discord.Member):
+    nickname = 'Ѧ'+ user.name
+    await client.change_nickname(user, nickname=nickname)
+    role = discord.utils.get(ctx.message.server.roles, name='Admin')
+    await client.add_roles(user, role)
+    r, g, b = tuple(int(x * 255) for x in colorsys.hsv_to_rgb(random.random(), 1, 1))
+    embed = discord.Embed(color = discord.Color((r << 16) + (g << 8) + b))
+    embed.set_author(name='Congratulations Message')
+    embed.add_field(name = '__Congratulations__',value ='**Congratulations for Admin.Hope you will be more active here. Thanks for your help and support.**',inline = False)
+    embed.set_image(url = 'https://preview.ibb.co/i1izTz/ezgif_5_e20b665628.gif')
+    await client.send_message(user,embed=embed)
+    await client.delete_message(ctx.message)
+
+
+@client.command(pass_context = True)
+async def sorry(ctx, *, msg = None):
+    if '@here' in msg or '@everyone' in msg:
+      return
+    if not msg: await client.say("Please Sorry")
+    await client.say('Sorry ' + msg + ' http://imgur.com/gallery/Dif2lYI')
+    return
+
+
+@client.command(pass_context = True)
+@commands.has_permissions(manage_messages=True)  
+async def clear(ctx, number):
+ 
+    if ctx.message.author.server_permissions.manage_messages:
+         mgs = [] #Empty list to put all the messages in the log
+         number = int(number) #Converting the amount of messages to delete to an integer
+    async for x in client.logs_from(ctx.message.channel, limit = number+1):
+        mgs.append(x)            
+       
+    try:
+        await client.delete_messages(mgs)          
+        await client.say(str(number)+' messages deleted')
+     
+    except discord.Forbidden:
+        await client.say(embed=Forbidden)
+        return
+    except discord.HTTPException:
+        await client.say('clear failed.')
+        return         
+   
+ 
+    await client.delete_messages(mgs)      
+
+ 
+@client.event
+async def on_message_delete(message):
+    if not message.author.bot:
+      channelname = 'information-log'
+      logchannel=None
+      for channel in message.server.channels:
+        if channel.name == channelname:
+          user = message.author
+      for channel in user.server.channels:
+        if channel.name == 'soyal-log':
+          logchannel = channel
+          r, g, b = tuple(int(x * 255) for x in colorsys.hsv_to_rgb(random.random(), 1, 1))
+          embed = discord.Embed(color = discord.Color((r << 16) + (g << 8) + b))
+          embed.set_author(name='Message deleted')
+          embed.add_field(name = 'User: **{0}**'.format(user.name),value ='UserID: **{}**'.format(user.id),inline = False)
+          embed.add_field(name = 'Message:',value ='{}'.format(message.content),inline = False)
+          embed.add_field(name = 'Channel:',value ='{}'.format(message.channel.name),inline = False)
+          await client.send_message(logchannel,  embed=embed)	
+		
+@client.command(pass_context = True)
+async def avatar(ctx, user: discord.Member=None):
+    if user is None:
+        r, g, b = tuple(int(x * 255) for x in colorsys.hsv_to_rgb(random.random(), 1, 1))
+        embed = discord.Embed(title=f'Avatar', description='Avatar is profile picture of a user in discord', color = discord.Color((r << 16) + (g << 8) + b))
+        embed.add_field(name='User: {}'.format(ctx.message.author.name), value='Avatar:', inline=True)
+        embed.set_thumbnail(url='https://cdn.discordapp.com/attachments/520159870448566287/520829749095038977/pubg.png') 
+        embed.set_image(url = ctx.message.author.avatar_url)
+        await client.say(embed=embed)
+    else:
+        r, g, b = tuple(int(x * 255) for x in colorsys.hsv_to_rgb(random.random(), 1, 1))
+        embed = discord.Embed(title=f'Avatar', description='Avatar is profile picture of a user in discord', color = discord.Color((r << 16) + (g << 8) + b))
+        embed.add_field(name='User: {}'.format(user.name), value='Avatar:', inline=True)
+        embed.set_thumbnail(url='https://cdn.discordapp.com/attachments/520159870448566287/520829749095038977/pubg.png') 
+        embed.set_image(url = user.avatar_url)
+        await client.say(embed=embed)
+
 client.run(os.getenv('Token'))
