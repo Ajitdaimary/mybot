97e6486d7e62fb7e50388295ffd86ca152a7d777
badsahah <45558399+Ajitdaimary@users.noreply.github.com>
2018-12-17 19:59:36 +0530

    Update online.py

--- online.py
+++ online.py
@@ -720,5 +720,17 @@ async def channelsetup(ctx):
     await client.create_channel(server, 'pokémon_commands',user)
     await client.create_channel(server, 'A_F_K', type=discord.ChannelType.voice)
 
+@client.command(pass_context=True)
+async def tweet(ctx, usernamename:str, *, txt:str):
+    url = f"https://nekobot.xyz/api/imagegen?type=tweet&username={usernamename}&text={txt}"
+    async with aiohttp.ClientSession() as cs:
+        async with cs.get(url) as r:
+            res = await r.json()
+            r, g, b = tuple(int(x * 255) for x in colorsys.hsv_to_rgb(random.random(), 1, 1))
+            embed = discord.Embed(color = discord.Color((r << 16) + (g << 8) + b))
+            embed.set_image(url=res['message'])
+            embed.title = "{} twitted: {}".format(usernamename, txt)
+            await client.say(embed=embed)
+
 	
 client.run(os.getenv('Token'))
