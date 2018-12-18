03cf6c768bbef528c46d1d910da99a5756df0c68
badsahah <45558399+Ajitdaimary@users.noreply.github.com>
2018-12-18 13:52:55 +0530

    Update online.py

--- online.py
+++ online.py
@@ -131,4 +131,20 @@ async def help(ctx):
       await client.say('I sent you the list of commands in a private message. Check your direct messages')
    
     
+
+@client.command(pass_context=True)
+async def tweet(ctx, usernamename:str, *, txt:str):
+    url = f"https://nekobot.xyz/api/imagegen?type=tweet&username={usernamename}&text={txt}"
+    async with aiohttp.ClientSession() as cs:
+        async with cs.get(url) as r:
+            res = await r.json()
+            r, g, b = tuple(int(x * 255) for x in colorsys.hsv_to_rgb(random.random(), 1, 1))
+            embed = discord.Embed(color = discord.Color((r << 16) + (g << 8) + b))
+            embed.set_image(url=res['message'])
+            embed.title = "{} twitted: {}".format(usernamename, txt)
+            await client.say(embed=embed)
+
+		
+
+
 client.run(os.getenv('Token')) 
