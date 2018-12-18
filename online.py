d1613aedb79e6c0de13d9175783fca68c761d56d
badsahah <45558399+Ajitdaimary@users.noreply.github.com>
2018-12-18 13:49:29 +0530

    Update online.py

--- online.py
+++ online.py
@@ -12,10 +12,13 @@ import time
 import datetime
 import requests
 import json
-import aiohttp
+import aiohttp		
+
 
 Forbidden= discord.Embed(title="Permission Denied", description="1) Please check whether you have permission to perform this action or not. \n2) Please check whether my role has permission to perform this action in this channel or not. \n3) Please check my role position.", color=0x00ff00)
-client = commands.Bot(description="Here is some command for you", command_prefix=commands.when_mentioned_or("!!"), pm_help = False)
+client = commands.Bot(description="MultiVerse Official Bot", command_prefix=commands.when_mentioned_or("m!"), pm_help = True)
+client.remove_command('help')
+
 
 
 async def status_task():
